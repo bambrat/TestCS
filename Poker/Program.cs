2294dc32019b5c5372eab26a981bd3ae3bfabe6a
Daniil <bambrat@yandex.ru>
2024-06-19 15:14:37 +0600

    0.0.2 На сдачу решения

--- Poker/Program.cs
+++ Poker/Program.cs
@@ -1,27 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading;
 
 namespace Poker
 {
     internal class Program
     {
-        static void Calc(List<int> list_)
+        static void Calculation(List<int> list_)
         {
             //среднее 
             var mean = list_.Sum() / list_.Count();
             
-            //Console.WriteLine($"сумма {sum}");
-            //Console.WriteLine($"среднее {mean}");
-
             List<int> list_c = new List<int> {};
-
-
-            //for (int i = 0; i < list_.Count; i++) {
-            //    list_c.Add(list_[i] - mean);
-            //}
 
             foreach (var item in list_)
             {
@@ -31,11 +21,6 @@ namespace Poker
             //кумулятивная сумма элементов
             int sum_c = 0;
             List<int> list_cs = new List<int> {};
-            //for (int i = 0; i < list_c.Count; i++)
-            //{
-            //   list_cs.Add(list_c[i] + sum_c);
-            //   sum_c = sum_c + list_c[i];
-            //}
 
             foreach (var item in list_c)
             {
@@ -44,33 +29,15 @@ namespace Poker
             }
 
             //ищем медиану
-            //for (int i = 0; list_cs.Count > i; i++)
-            //{
-            //    Console.WriteLine($"{list_c[i]} \t {list_cs[i]}");
-            //}
-
-            //foreach (var item in list_cs)
-            //{
-            //    Console.WriteLine($"\t{item}");
-            //}
-
             var med = ((int)Median(list_cs));
-
-            Console.WriteLine($"медианна {med}");
 
             for (int i = 0; i < list_cs.Count; i++)
             {
                 list_cs[i] = Math.Abs(list_cs[i] - med);
             }
 
-            //for (int i = 0; list_cs.Count > i; i++)
-            //{
-            //    Console.WriteLine($"\t {list_cs[i]}");
-            //}
-
             var ret = list_cs.Sum();
             Console.WriteLine($"{ret}");
-
         }
 
         static decimal Median(List<int> source)
@@ -81,26 +48,21 @@ namespace Poker
             int count = temp.Length;
             if (count % 2 == 0)
             {
-                // count is even, average two middle elements
                 int a = temp[count / 2 - 1];
                 int b = temp[count / 2];
                 return (a + b) / 2m;
             }
             else
             {
-                // count is odd, return the middle element
                 return temp[count / 2];
             }
         }
 
-
-
-        static void Main(string[] args)
+        static void Main()
         {
             while (true)
             {
                 Console.WriteLine("Input:");
-
                 List<int> list_int = new List<int> { };
 
                 try
@@ -117,11 +79,8 @@ namespace Poker
                     Console.WriteLine($"The number of chips cannot be negative or equal to 0");
                     continue;
                 }
-
-                Calc(list_int);
-
+                Calculation(list_int);
             }
-            
         }
     }
 }
